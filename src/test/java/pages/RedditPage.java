2bf02081245dce7d02acc11ab3cf40faef903a39
Thomas Knee <thomas@spiritai.com>
2018-12-30 15:38:13 +0000

    add RedditContentArea assertion

--- src/test/java/pages/RedditPage.java
+++ src/test/java/pages/RedditPage.java
@@ -1,8 +1,17 @@
 package pages;
 
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static utils.extensions.WebElementExtensions.*;
+
 public class RedditPage extends SearchResultsPage {
 
+    @FindBy(id = "SHORTCUT_FOCUSABLE_DIV")
+    private WebElement redditContentArea;
+
     public void assertIAmOnRedditPage() {
+        weElementIsDisplayed(redditContentArea);
         validatePageTitle("reddit:");
         validatePageUrl("https://www.reddit.com");
         validatePageSource("Reddit");
